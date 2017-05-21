2140d15d271c132cf8075bdcb6f9c1be425a1725
JacobZajac <jacobczajac@gmail.com>
2017-05-21 16:43:49 -0700

    Update TruthColumn.java

--- JZajac TruthColumn/src/TruthColumn.java
+++ JZajac TruthColumn/src/TruthColumn.java
@@ -6,8 +6,10 @@
  * 
  * Introduction
  *
- * This assignment asked us to implement various logical operators on either one or two arrays of boolean data.
- * An example is the And operator, which is boolean from array 1 and boolean from array 2.  An example of the And operator is as follows:
+ * This assignment asked us to implement various logical operators on 
+ * either one or two arrays of boolean data.
+ * An example is the And operator, which is boolean from array 1 and boolean from array 2.  
+ * An example of the And operator is as follows:
  * Value 1 | Value 2 | Value 1 And 2
  *    T    |    T    |     T
  *    T    |    F    |     F
@@ -32,9 +34,11 @@
  * 
  * Each operator has its own method associated with it.
  * Depending on the operator, the method takes either 1 or 2 arrays of boolean values.
- * Using for loops, the program iterates through each item in single array operators, or through corresponding items 
+ * Using for loops, the program iterates through each item in single array operators, 
+ * or through corresponding items 
  * in two array operators.  
- * In some methods, such as And, applying the operator to the data creates a new piece of data.
+ * In some methods, such as And, applying the operator to the data creates a new piece 
+ * of data.
  * In these cases, that data is put into a new array, which is then returned.
  * In other methods, such as Valid, the program checks for a condition.
  * Depending on the condition, a single value, true or false, is returned.
@@ -42,12 +46,13 @@
  * Challenges
  * 
  * The largest challenge was learning the behavior of each logical operator.
- * However, once the behaviors were learned, some of them were actually very easy to implement, 
- * such as Consistent.
+ * However, once the behaviors were learned, some of them were actually very easy 
+ * to implement, such as Consistent.
  * 
  * Acknowledgments
  * 
  * The Stanford Logic website was very helpful in explaining the logical operators and their behaviors.
+ * A special Thank You to Mr. Pavan Desikan for all his help with checking my code and documentation.
  * 
  * 
  */
@@ -72,7 +77,7 @@ public class TruthColumn {
 	}
 	
 	//Prints the contents of an array
-	//@param Arr The array to be printed
+	// @param Arr The array to be printed
 	public static void printArray(boolean[] Arr) {
 	      for (int i = 0; i < Arr.length; i++) {
 	         if (i > 0) {
@@ -83,9 +88,9 @@ public class TruthColumn {
 	  }
 	
 	//Returns an array which contains object from array 1 *and* object from array 2.
-	//@param Arr1 The first array of booleans
-	//@param Arr2 The second array of booleans
-	//@return Returns Arr1 and Arr2 
+	// @param Arr1 The first array of booleans
+	// @param Arr2 The second array of booleans
+	// @return Returns Arr1 and Arr2 
 	public static boolean[] and(boolean[] Arr1, boolean[] Arr2) {
 		boolean[] retval = new boolean[Arr1.length];
 		for (int pos = 0; pos < retval.length; pos++) {
@@ -96,9 +101,9 @@ public class TruthColumn {
 	}
 	
 	//Returns an array which contains object from array 1 *or* object from array 2.
-	//@param Arr1 The first array of booleans
-	//@param Arr2 The second array of booleans
-	//@return Returns Arr1 or Arr2
+	// @param Arr1 The first array of booleans
+	// @param Arr2 The second array of booleans
+	// @return Returns Arr1 or Arr2
 	public static boolean[] or(boolean[] Arr1, boolean[] Arr2) {
 		boolean[] retval = new boolean[Arr1.length];
 		for (int pos = 0; pos < retval.length; pos++) {
@@ -109,8 +114,8 @@ public class TruthColumn {
 	}
 	
 	//Returns an array which is the negated version of an array.
-	//@param Arr1 The original array of booleans
-	//@return Returns negated version of Arr1 
+	// @param Arr1 The original array of booleans
+	// @return Returns negated version of Arr1 
 	public static boolean[] negate(boolean[] Arr1) {
 		boolean[] retval = new boolean[Arr1.length];
 		for (int pos = 0; pos < retval.length; pos++) {
@@ -120,9 +125,9 @@ public class TruthColumn {
 	}
 	
 	//Returns an array where the object is true if an object in Arr1 implies an object inArr2.
-	//@param Arr1 The first array of booleans
-	//@param Arr2 The second array of booleans
-	//@return Returns an array of values for Arr1 implies Arr2
+	// @param Arr1 The first array of booleans
+	// @param Arr2 The second array of booleans
+	// @return Returns an array of values for Arr1 implies Arr2
 	public static boolean[] imply(boolean[] Arr1, boolean[] Arr2) {
 		boolean[] retval = new boolean[Arr1.length];
 		for (int pos = 0; pos < retval.length; pos++) {
@@ -133,9 +138,9 @@ public class TruthColumn {
 	}
 	
 	//Returns an array which represents the biconditional.  It is true only when the object in Arr1 equals the object in Arr2.
-	//@param Arr1 The first array of booleans
-	//@param Arr2 The second array of booleans
-	//@return Returns the biconditional of Arr1 and Arr2
+	// @param Arr1 The first array of booleans
+	// @param Arr2 The second array of booleans
+	// @return Returns the biconditional of Arr1 and Arr2
 	public static boolean[] biconditional(boolean[] Arr1, boolean[] Arr2) {
 		boolean[] retval = new boolean[Arr1.length];
 		for (int pos = 0; pos < retval.length; pos++) {
@@ -146,8 +151,8 @@ public class TruthColumn {
 	}
 	
 	//Returns a boolean which represents the validity of the array.
-	//@param Arr1 The first array of booleans
-	//@return Returns true if every value in Arr1 is true, false otherwise
+	// @param Arr1 The first array of booleans
+	// @return Returns true if every value in Arr1 is true, false otherwise
 	public static boolean valid(boolean[] Arr1) {
 
 		for (int pos = 0; pos < Arr1.length; pos++) {
@@ -160,8 +165,8 @@ public class TruthColumn {
 	}
 	
 	//Returns a boolean which represents the unsatisfiability of the array.
-	//@param Arr1 The first array of booleans
-	//@return Returns false if at least 1 value in Arr1 is true, returns false otherwise
+	// @param Arr1 The first array of booleans
+	// @return Returns false if at least 1 value in Arr1 is true, returns false otherwise
 	public static boolean unsatisfiable(boolean[] Arr1) {
 
 		for (int pos = 0; pos < Arr1.length; pos++) {
@@ -174,8 +179,8 @@ public class TruthColumn {
 	}
 	
 	//Returns a boolean which represents the contingency of the array.
-	//@param Arr1 The first array of booleans
-	//@return Returns true if there is at least 1 true value and at least 1 false value, false otherwise
+	// @param Arr1 The first array of booleans
+	// @return Returns true if there is at least 1 true value and at least 1 false value, false otherwise
 	public static boolean contingent(boolean[] Arr1) {
 		
 		return (!valid(Arr1) && !unsatisfiable(Arr1));
@@ -183,17 +188,17 @@ public class TruthColumn {
 	}
 	
 	//Returns a boolean which represents the equivalency of two arrays.
-	//@param Arr1 The first array of booleans
-	//@param Arr2 The second array of booleans
-	//@return Returns true if Arr1 is the same as Arr2, false otherwise
+	// @param Arr1 The first array of booleans
+	// @param Arr2 The second array of booleans
+	// @return Returns true if Arr1 is the same as Arr2, false otherwise
 	public static boolean equivalent(boolean[] Arr1, boolean[] Arr2) {
 		return (Arrays.equals(Arr1, Arr2));
 	}
 	
 	//Returns a boolean value determined by if Arr1 entails Arr2.
-	//@param Arr1 The first array of booleans
-	//@param Arr2 The second array of booleans
-	//@return Returns false if there is any instance when Arr1 is true but Arr2 is false, returns true otherwise
+	// @param Arr1 The first array of booleans
+	// @param Arr2 The second array of booleans
+	// @return Returns false if there is any instance when Arr1 is true but Arr2 is false, returns true otherwise
 	public static boolean entails(boolean[] Arr1, boolean [] Arr2) {
 	
 		for (int pos = 0; pos < Arr1.length; pos++) {
@@ -205,9 +210,9 @@ public class TruthColumn {
 	}
 	
 	//Returns a boolean value determined by if Arr1 and Arr2 are consistent.
-	//@param Arr1 The first array of booleans
-	//@param Arr2 The second array of booleans
-	//@return Returns true if the array of Arr1 and Arr2 is valid, false otherwise
+	// @param Arr1 The first array of booleans
+	// @param Arr2 The second array of booleans
+	// @return Returns true if the array of Arr1 and Arr2 is valid, false otherwise
 	public static boolean consistent(boolean[] Arr1, boolean[] Arr2) {
 		return !unsatisfiable(and(Arr1, Arr2));
 	}
